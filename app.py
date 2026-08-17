6566c4294c9832dd58b96cfa14335533d20f66b1
Janarthanan P <janarthanan12102005@gmail.com>
2026-08-17 18:34:05 +0530

    Add sitemap and robots.txt routes to Flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -33,10 +33,45 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/sitemap.xml")
+def sitemap():
+    pages = [
+        "https://aruvonlearningsolutions.me/",
+        "https://aruvonlearningsolutions.me/about",
+        "https://aruvonlearningsolutions.me/courses",
+        "https://aruvonlearningsolutions.me/training-experience",
+        "https://aruvonlearningsolutions.me/student-feedback",
+        "https://aruvonlearningsolutions.me/contact",
+    ]
+
+    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+    for page in pages:
+        xml += "    <url>\n"
+        xml += f"        <loc>{page}</loc>\n"
+        xml += "    </url>\n"
+
+    xml += "</urlset>"
+
+    return Response(xml, mimetype="application/xml")
+
+
+@app.route("/robots.txt")
+def robots():
+    content = """User-agent: *
+Allow: /
+
+Sitemap: https://aruvonlearningsolutions.me/sitemap.xml
+"""
+
+    return Response(content, mimetype="text/plain")
+
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
-    app.run(debug=False , host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
